a405efb553068aef497d7b5c305e77de2f4cdf91
Volodymyr Sauliak <3429638@gmail.com>
2020-05-20 18:17:02 +0300

    julpfile.js added

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ group :jekyll_plugins do
 #  gem 'jekyll-sitemap'
   gem 'jekyll-seo-tag'
   gem "jekyll-paginate-v2", ">= 3.0"
+ # gem 'octopress-minify-html'
 end
 
 gem 'liquid', github: 'Shopify/liquid', branch: 'master'
--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,25 @@ var browserSync = require("browser-sync").create();
 var load = require("gulp-load-plugins")();
 var autoprefixer = require("autoprefixer");
 var sass = require("gulp-sass");
-
 sass.compiler = require("node-sass");
 var sassPaths = [
   "assets",
   "_sass",
 ];
 const htmlmin = require("gulp-htmlmin");
+const minify = require('gulp-minify');
 
+/* compress JS files */
+gulp.task('compressjs', function () {
+  return gulp.src('_site/js/*.js')
+    .pipe(minify())
+    .pipe(gulp.dest('_site/js'));
+});
+
+/* compress HTML files */
 gulp.task("minify", () => {
   return gulp
-    .src("_site/*/*.html")
+    .src("_site/**/*.html")
     .pipe(
       htmlmin({
         collapseWhitespace: true,
@@ -26,6 +34,7 @@ gulp.task("minify", () => {
     .pipe(gulp.dest("_site"));
 });
 
+/* compress CSS file */
 gulp.task("sass", function () {
   return gulp
     .src("./_site/assets/main.css")
@@ -36,21 +45,17 @@ gulp.task("sass", function () {
 // Task for building blog when something changed:
 gulp.task("jekyll", shell.task("bundle exec jekyll build"));
 gulp.task("jekyll-s", shell.task("bundle exec jekyll serve"));
-// If you don't use bundle:
-// gulp.task('build', shell.task(['jekyll serve']));
-// If you use  Windows Subsystem for Linux (thanks @SamuliAlajarvela):
-// gulp.task('build', shell.task(['bundle exec jekyll serve --force_polling']));
  
 // Task for serving blog with Browsersync
 
 gulp.task("serve", function () {
-//    browserSync.init({
-//    server: { baseDir: "./" },
-//    ui: { port: 4001 },
-//    port: 4000,
-//  });
-//  gulp.watch("_site/**/*.*").on("change", browserSync.reload);
+    browserSync.init({
+    server: { baseDir: "./" },
+    ui: { port: 4001 },
+    port: 4000,
+  });
+  gulp.watch("_site/**/*.*").on("change", browserSync.reload);
 });
 /* gulp.task("sass", sass); */
-gulp.task("build", gulp.series("jekyll", "sass"));
+gulp.task("build", gulp.series("jekyll", "minify", "compressjs", "sass"));
 gulp.task("default", gulp.parallel("jekyll-s", "serve"));
